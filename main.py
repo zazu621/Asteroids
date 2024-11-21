7dd12307ed4d1dfb8ae6023b734275957c53a08c
zazu621 <mia.maria.huss@gmail.com>
2024-11-21 08:45:15 +0100

    latest

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
-    print("Starting asteroids!")   
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    print("Driver: ", pygame.display.get_driver())
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
-        screen.fill("black")
-       
-  
-    
-    
-    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))
+        player.draw(screen)
+        pygame.display.flip()
+
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
+
